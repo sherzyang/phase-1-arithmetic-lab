0f678f30a143efaf2427130141be6f35ee0725c2
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-11 22:47:04 -0800

    Update.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply;
+
+multiply = num1 *= num2;
+
+let random;
+
+random = Math.round(Math.random() * 10);
+
+let mod; 
+let num3 = 29;
+let num4 = 5;
+
+mod = num3 %= num4;
+
+let max; 
+
+max = Math.max(1,2,3,4,5,6,20);
